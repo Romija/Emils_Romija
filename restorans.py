b1e56859061f412bf1b0cd2b30e1652260a7e4f7
Romija <romija.suhanova@svg.lv>
2025-04-11 08:43:48 +0300

    Update restorans.py

--- restorans.py
+++ restorans.py
@@ -10,7 +10,7 @@ class Noliktava(Produkts):
         super().__init__(nosaukums, skaits, dienu_skaits, minimalais_skaits)
 
     def info(self):
-        print(f"{self.nosaukums}: {self.skaits}")
+        return f"{self.nosaukums}: {self.skaits} kg"
 
     def pietrukst(self):
         return self.skaits<self.minimalais_skaits
@@ -33,17 +33,36 @@ class Pasutijums:
 
     def saglabat_faila(self, faila_nosaukums = 'saturs.txt'):
         with open(faila_nosaukums, "a", encoding = 'utf8') as fails:
-            fails.write(self) #######################
+            fails.write(f"{self.nosaukums}: {self.skaits} kg")
 
+    @classmethod
     def nolasit_saturu(self, faila_nosaukums):
         try:
             with open(faila_nosaukums, "r", encoding = "utf8") as fails:
-                fails.readlines(faila_nosaukums)
-
-                '''
-                if not in fails:
-                    print("Failā nav satura!")
-                    '''
-                    
+                saturs = fails.readlines()
+                if not saturs:
+                    return "Failā nav satura!"
+                return "".join(saturs) 
         except FileNotFoundError:
             print("Fails nepastāv!")
+
+class Tirgotajs:
+    pass
+
+tirgotaji = {
+    "Maiznieks" : Tirgotajs("Maiznieks", "Maize", 5),
+    "Miesnieks" : Tirgotajs("Miesnieks", "Gaļa", 15),
+    "Piena kombināts" : Tirgotajs("Piena kombināts", "Piens", 20),
+    "Šokolādes fabrika" : Tirgotajs("Šokolādes fabrika", "Šokolāde", 3),
+    "Miltu fabrika" : Tirgotajs("Miltu fabrika", "Milti", 5),
+    "Kartupeļu lauks" : Tirgotajs("Kartupeļu lauks", "Kartupeļi", 30),
+    "Siernīca" : Tirgotajs("Siernīca", "Siers", 15)
+}
+
+maize = Produkts("Maize",7,2,5)
+gala = Produkts("Gaļa",20,1,25)
+piens = Produkts("Piens",5,2,7)
+sokolade = Produkts("Šokolāde", 2, 3, 2)
+milti = Produkts("Milti", 2, 13, 3)
+kartupeli = Produkts("Kartupeļi", 35, 3, 30)
+siers = Produkts("Siers",10,2,8)
