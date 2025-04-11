836944654f0022660a3224732fb40cf82eae034b
EmilsSermulisSVG <144447091+EmilsSermulisSVG@users.noreply.github.com>
2025-04-11 08:44:04 +0300

    Update projekts_02_04.py

--- projekts_02_04.py
+++ projekts_02_04.py
@@ -1,41 +1,41 @@
-from abc import ABC
-class Produkts(ABC):
+from abc import ABC, abstractmethod
+class Produkts(ABC): #
     def __init__(self,nosaukums,skaits,dienu_skaits,minimalais_skaits):
         self.nosaukums = nosaukums
         self.skaits = skaits
         self.dienu_skaits = dienu_skaits
         self.minimalais_skaits = minimalais_skaits
 
+    @abstractmethod
+    def info(self):
+        pass
 
-siers = Produkts("Siers",10,2,8)
-piens = Produkts("Piens",5,2,7)
-maize = Produkts("Maize",7,2,5)
-
-
-class SvaigsProdukts(Produkts):
+class SvaigsProdukts(Produkts): #Manto klasi "Produkts"
     def __init__(self,nosaukums,skaits,dienu_skaits,minimalais_skaits):
         super().__init__(nosaukums,skaits,dienu_skaits,minimalais_skaits)
     
 
-
-class Tirgotajs:       
-    def __init__(self,tirgotajs,min_pasutijuma_daudzums):
+class Tirgotajs: # klase Tirgotājs      
+    def __init__(self,tirgotajs,produkts,min_pasutijuma_daudzums):
         self.tirgotajs = tirgotajs
+        self.produkts = produkts
         self.min_pasutijuma_daudzums = min_pasutijuma_daudzums
 
-        if self.adresats = adresats:
-            pass
-        else:
-            print("Nepareizais adresāts!")
 
-    def pienemt_pasutijumu(self):
+    def pienemt_pasutijumu(self): # funkcija pārbauda vai lietotājs var pasūtīt produktus, vai nav mazāk par minimālo daudzumu
         if pasutijuma_daudzums >= min_pasutijuma_daudzums:
            print("Pasūtījums ir pieņemts")
-        elif minimalais_skaits > min_pasutijuma_daudzums:
-            print("Pasūtījums netiek pieņemts, jo pasūtījuma daudzums ir mazāks par minimālo daudzumu")
+        elif min_pasutijuma_daudzums > pasutijuma_daudzums:
+            print(f"Pasūtījums netiek pieņemts, jo pasūtījuma daudzums ir mazāks ({self.pasutijuma_daudzums}) par minimālo daudzumu ({self.min_pasutijuma_daudzums})")
+        else:
+            print("Ievadiet pareizus datus!")
         
+        
+#objektu izveide
+tirgotajs1 = Tirgotajs("Maiznieks","Maize",5)
+tirgotajs2 = Tirgotajs("Miesnieks","Gaļa",3)
+tirgotajs3 = Tirgotajs("Piena kombināts","piens",7)
 
-
-
-maiznieks = Tirgotajs("Maiznieks")
-
+maize = Produkts("Maize",7,2,5)
+gala = Produkts("Gaļa",10,2,3)
+piens = Produkts("Piens",5,2,7)
